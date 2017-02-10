076ede12cd5ea39038654fe24429289fbc95b846
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2017-02-10 21:50:17 +0100

    Add password and bootnode configuration

--- ethereum/start.sh
+++ ethereum/start.sh
@@ -2,6 +2,9 @@
 if [ ! -d "/root/.ethereum/geth" ]; then
     echo "writing new genesis block";
     /geth init /root/genesis.json;
+    /geth --password /root/password account new
 fi
 
-/geth;
+/geth --mine \
+    --minerthreads=1 \
+    --bootnodes=enode://29cddb14c597c2c285f3ccdb2505798777cf42c7b22ffad141f60185de93a6602b4f87a40ef5779a199c7b83772ee5edb2522b7ec35eac49f50efde3f92aafaf@185.110.174.70:30303;
