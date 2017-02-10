28325332f175c013c7d3c58449c20ff2647887f3
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2017-02-10 23:07:29 +0100

    Add static-nodes.json

---
+++ api/src/index.js
@@ -0,0 +1,6 @@
+var restify = require('restify');
+var Web3 = require('web3');
+
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+console.log(web3.isConnected());
--- ethereum/start.sh
+++ ethereum/start.sh
@@ -3,8 +3,8 @@ if [ ! -d "/root/.ethereum/geth" ]; then
     echo "writing new genesis block";
     /geth init /root/genesis.json;
     /geth --password /root/password account new
+    mv /root/static-nodes.json /root/.ethereum/static-nodes.json
 fi
 
 /geth --mine \
-    --minerthreads=1 \
-    --bootnodes=enode://419826c720ee949c7efde76e53ab0403ff9b8feae94a6b128daf0a91fb1d69d8e3a36466acb587cc7315b33cce58ae913cb29d52e0a66f077b171624ac974aa9@185.110.174.70:30303;
+    --minerthreads=1;
